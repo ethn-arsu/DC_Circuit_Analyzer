35d7f7c13393e25ec232341c1a1cbbcb0a500c06
Ethan Arsua <ethan_arsua@dlsu.edu.ph>
2026-07-30 23:58:04 +0800

    Calculation engine file

--- src/circuit.c
+++ src/circuit.c
@@ -1,3 +1,18 @@
-//
-// Created by earsu on 7/30/2026.
-//
+#include "circuit.h"
+
+
+double calculate_series_resistance(
+        double r1,
+        double r2
+)
+{
+    return r1 + r2;
+}
+
+double calculate_current(
+        double voltage,
+        double resistance
+)
+{
+    return voltage / resistance;
+}
